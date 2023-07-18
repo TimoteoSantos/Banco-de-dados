3504fa392c53ceef4cfb3f1bf56adb82164f1057
timoteosantos <santostimoteo21@gmail.com>
2023-07-18 06:06:52 -0300

    iniciando estudos sql

---
+++ BASICO SQL.sql
@@ -0,0 +1,9 @@
+/*seleciona um banco de dados*/
+USE tichronoscom_sisnotas;
+/*mostra as tabelas de um banco de dados*/
+SHOW TABLES;
+/*faz uma consulta no banco de dados*/
+SELECT * FROM vendas;
+
+
+
