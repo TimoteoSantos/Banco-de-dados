2ee620df527bfb0f959620e66892879a51f9bc53
timoteo santos <santostimoteo21@gmail.com>
2023-07-21 09:31:28 -0300

    organizando pastas

--- comandos basicos/BASICO SQL.sql
+++
@@ -1,9 +0,0 @@
-/*seleciona um banco de dados*/
-USE tichronoscom_sisnotas;
-/*mostra as tabelas de um banco de dados*/
-SHOW TABLES;
-/*faz uma consulta no banco de dados*/
-SELECT * FROM vendas;
-
-
-
